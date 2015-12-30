6f65653cb7c4b70e0fff1a63b57641fda8294519
qianfeng <1418755267@qq.com>
2015-12-30 22:34:13 +0800

    Initial Commit

---
+++ 1230Test/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  1230Test
+//
+//  Created by qianfeng on 15/12/30.
+//  Copyright © 2015年 TaiYang. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
